2d75c4b7b68721230454929396f2f59634e811fa
Rushikesh Akhare <rushikesh.akhare@globallogic.com>
2019-07-17 11:54:55 +0530

    Fonts CHANGED

--- index.blade.php
+++ index.blade.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TEST</title>
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap" rel="stylesheet">
     <style>
+        body
+        {
+            font-family: 'Titillium Web', sans-serif;
+        }
 .fullpage
 {
     width:100%;
