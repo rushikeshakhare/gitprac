b5c1ede33473cdd0c584ef9c91cbeff317d3cf95
Rushikesh Akhare <rushikesh.akhare@globallogic.com>
2019-07-17 12:29:34 +0530

    welcome to globalLogic i ngp

--- index.blade.php
+++ index.blade.php
@@ -29,7 +29,7 @@
    <div class="fullpage">
        <h1>Helloworld</h1>
        <p>Welcome to this happy world</p>
-       <p>Welcome to GlobalLogic</p>
+       <p>Welcome to GlobalLogic In Nagpur</p>
    </div>
 </body>
 </html>
