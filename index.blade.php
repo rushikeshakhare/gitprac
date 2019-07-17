762ee54aa9ae0e6f4d6cb3c8f5da5d628028c89b
Rushikesh Akhare <rushikesh.akhare@globallogic.com>
2019-07-17 11:52:52 +0530

    New COlors Added

--- index.blade.php
+++ index.blade.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>TEST</title>
+    <style>
+.fullpage
+{
+    width:100%;
+    height:100%;
+    position:fixed;
+    left:0;
+    top:0;
+    background:#37474f;
+    padding:5%;
+    color:white;
+}
+    </style>
 </head>
 <body>
-    <h1>Helloworld</h1>
+   <div class="fullpage">
+       <h1>Helloworld</h1>
+   </div>
 </body>
 </html>
